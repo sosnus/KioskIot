04509d57408231428581b7eaae8ea0ba16193fa0
Sosnus <sosnus15@gmail.com>
2019-10-09 15:05:34 +0200

    new layout, open grafana

--- KioskIot/MainPage.xaml.cs
+++ KioskIot/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +26,21 @@ namespace KioskIot
         public MainPage()
         {
             this.InitializeComponent();
+            Button_Click(null, null);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            WebView1.Navigate(new Uri(@"http://40.85.113.214:3000/?orgId=1"));
+            tblPageStatus.Text =". Loading... .";
+            tblPageStatus.Foreground = new SolidColorBrush(Colors.Red);
+        }
+
+        private void WebView1_LoadCompleted(object sender, NavigationEventArgs e)
+        {
+            tblPageStatus.Text = ". Done! .";
+            tblPageStatus.Foreground = new SolidColorBrush(Colors.Green);
+            //  WebView1.Navigate(new Uri(@"http://40.85.113.214:3000/?orgId=1"));
         }
     }
 }
